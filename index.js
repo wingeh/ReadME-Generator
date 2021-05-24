54c8d035008100874f82df769e4a1c48c14ef565
Andrew <wing_andrew@hotmail.com>
2021-05-23 21:03:33 -0400

    Adding development files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//ReadME Generator
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generator = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

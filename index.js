a6135883bf08825b1ed514b5a13a08bc8a8abbe0
Andrew <wing_andrew@hotmail.com>
2021-05-29 18:37:29 -0400

    completing write and initializing functions

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ const generator = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: (value) => { if (value) { return true } else { return 'Input not detected' } }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('Your README file has been generated!')
+    );
+  }
+  
+  
+  // TODO: Create a function to initialize app
+  function init() {
+    inquirer.prompt(questions)
+      .then(function (data) {
+        writeToFile('./output/README.md', generator(data));
+      })
+  }
 
 // Function call to initialize app
 init();

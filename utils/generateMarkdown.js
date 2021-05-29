8e6123635d2c85dae0470e2b20c563e554c1554d
Andrew <wing_andrew@hotmail.com>
2021-05-29 18:43:37 -0400

    Adding function to return license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+if (license === "The MIT License") {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
 
+} else if (license === 'The GNU GPL v2 License') {
+  return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)"
+
+} else if (license === 'The GNU GPL v3 License') {
+  return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)"
+
+} else if (license === 'Apache License') {
+  return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+
+} else {
+  return ""
+}
+
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
